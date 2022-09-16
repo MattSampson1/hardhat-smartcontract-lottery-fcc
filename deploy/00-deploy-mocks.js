8907b062e7d5279f7c2eb31370cb6dde4f029d71
Matt Sampson <matt@LAPTOP-U78TJCMT.localdomain>
2022-09-16 12:37:31 -0700

    initial commit

---
+++ deploy/00-deploy-mocks.js
@@ -0,0 +1,29 @@
+const { getNamedAccounts, deployments, network, ethers } = require("hardhat")
+
+const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK per request
+const GAS_PRICE_LINK = 1e9 // LINK per gas. Calculated value based on the gas price of the chain
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    args = [BASE_FEE, GAS_PRICE_LINK]
+    // If we are on a local development network, we need to deploy mocks!
+    if (chainId == 31337) {
+        log("Local network detected! Deploying mocks...")
+        await deploy("VRFCoordinatorV2Mock", {
+            from: deployer,
+            log: true,
+            args: args,
+        })
+
+        log("Mocks Deployed!")
+        log("----------------------------------------------------------")
+        log("You are deploying to a local network, you'll need a local network running to interact")
+        log(
+            "Please run `yarn hardhat console --network localhost` to interact with the deployed smart contracts!"
+        )
+        log("----------------------------------------------------------")
+    }
+}
+module.exports.tags = ["all", "mocks"]
